ecaac60c86b5df65fd5198883a434ae079efa1b6
Irene <Irene@LAPTOP-AHAM5SIA>
2016-03-16 14:51:54 +0100

    modifiche

--- Lab2_SpellChecker/src/it/polito/tdp/spellchecker/model/Dictionary.java
+++ Lab2_SpellChecker/src/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 public class Dictionary {
 	List<String> dizionario = new LinkedList<String>();
-	
-	
+
+
 
 	public List<String> getDizionario() {
 		return dizionario;
 	}
 
 	public void loadDictionary() {
-		
+
 	}
 	//Questo metodo non va implementato in questa classe. (Vedere esercizio 1.5).
-	
+
 	public List<RichWord> spellCheckText(List<String> inputTextList){
-		
+
 		List<RichWord> list = new LinkedList<RichWord>();
 		/*
 		boolean corretto = false;
@@ -27,35 +27,35 @@ public class Dictionary {
 				corretto = true;
 			list.add(new RichWord (stemp, corretto));
 		}
-		*/
+		 */
 		int inizio = 0;
 		int fine = dizionario.size();
-		int medio = dizionario.size()/2;
 		
 		boolean corretto = false;
 		for (String stemp: inputTextList){
+			
 			while (inizio!=fine){
+				int medio = (fine - inizio)/2;
+				System.out.println(inizio + " " + medio+ " "+ fine);
 				if (stemp.compareTo(dizionario.get(medio))==0){
 					corretto=true;
-					break;}
-				else if (stemp.compareTo(dizionario.get(medio))>0)
-					{inizio=medio;
-					medio=fine*(3/4);}
-					else {fine=medio;
-					medio=medio/2;
-					}
-				
+					break;
+				} else if (stemp.compareTo(dizionario.get(medio))>0){
+					inizio=medio;
+				} else {
+					fine=medio;
+				}
 			}
-			
-			
-			
-			
-			
-		/*	
-			
+
+
+
+
+
+			/*	
+
 			if(stemp.compareTo(dizionario.get(medio)) == 0)
 				corretto = true;
-				
+
 			else if(stemp.compareTo(dizionario.get(medio)) > 0){
 				for(int i = medio + 1; i < dizionario.size(); i ++){
 					if(stemp.compareTo(dizionario.get(i)) == 0){
@@ -72,12 +72,12 @@ public class Dictionary {
 					}		
 				}
 			}
-	*/			
-		list.add(new RichWord (stemp, corretto));
+			 */			
+			list.add(new RichWord (stemp, corretto));
 		}
-		
+
 		return list;
-		
+
 	}
 	//Implementare il metodo per poter eseguire il controllo ortografico del testo in input.
 
